c5c2c9f0d4dda57f8fe45a42283fa97488f22bd0
Timothy <105624091+tjm-teo@users.noreply.github.com>
2023-03-08 00:09:05 -0500

    Add files via upload
    Setup for first question complete

--- Assignment-1/MaxMeanSubarray.java
+++ Assignment-1/MaxMeanSubarray.java
@@ -1,1 +1,13 @@
+/**
+ * Author: Timothy Jalen Melendez
+ * Question: MaxMeanSubarray
+ * Technique:
+ * Time Complexity:
+ * Space Complexity:
+ */
+public class MaxMeanSubarray {
 
+    public static void main(String[] args){
+
+    }
+}
