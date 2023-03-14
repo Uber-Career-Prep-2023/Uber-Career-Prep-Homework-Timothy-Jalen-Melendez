1edafdc46d1700e199d17aa8531e8a0c30bf2513
Timothy <105624091+tjm-teo@users.noreply.github.com>
2023-03-13 17:08:06 -0700

    Add files via upload

--- Assignment-1/ReverseVowels.java
+++ Assignment-1/ReverseVowels.java
@@ -2,17 +2,44 @@
  * Author: Timothy Jalen Melendez
  * Question: ReverseVowels
  * Given a string, reverse the order of the vowels in the string.
- * Technique:
+ * Technique: Hash the elements
  * it is Fixed-size sliding window.
  * Time Complexity: O(n)
  * Space Complexity: O(n)
  */
+import java.lang.StringBuilder;
+import java.util.HashSet;
+import java.util.Stack;
 public class ReverseVowels {
 
-    private char[] vowels = {'a','A','e','E','i','I','o','O','u','U'};
     public static String reverse(String s){
-        String temp = "";
-        return temp;
+        StringBuilder word = new StringBuilder();
+        HashSet<Character> set = new HashSet<Character>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        set.add('A');
+        set.add('E');
+        set.add('I');
+        set.add('O');
+        set.add('U');
+        Stack<Character> tracker = new Stack<Character>();
+        for(int i = 0; i < s.length(); ++i){
+            if(set.contains(s.charAt(i))){
+                tracker.push(s.charAt(i));
+            }
+        }
+        for(int k = 0; k < s.length(); ++k){
+            if(set.contains(s.charAt(k))){
+                word.append(tracker.pop());
+            }
+            else{
+                word.append(s.charAt(k));
+            }
+        }
+        return word.toString();
     }
 
     public static void main(String[] args){
@@ -29,7 +56,7 @@ public class ReverseVowels {
         System.out.println(reverse("Uber Career Prep"));
         System.out.println(reverse("xyz"));
         System.out.println(reverse("flamingo"));
-
+        System.out.println(reverse(""));
 
 
 
